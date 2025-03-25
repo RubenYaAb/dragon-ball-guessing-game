63bc19ddeec24cdb853e4eccbc3b06c4283c4991
Ruben <ruben.ya.ab@gmail.com>
2025-03-25 17:32:11 +0100

    Copyright borrado

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,6 @@ export default function Footer(): JSX.Element {
   return (
     <footer className="footer">
       <p>Desarrollado por Rubén Yáñez Aballe</p>
-      <p>Todos los derechos reservados &copy;</p>
       <p>Contacto:<a href="mailto:magnes-dev@outlook.com">magnes-dev@outlook.com</a></p>
       <p>
         API:
